4cb37069f1e18ecb6577d0b9e4af2d023e965073
idzafic1 <idzafic1@etf.unsa.ba>
2025-10-25 13:00:02 +0200

    My little C# code

---
+++ MyProject/Math.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace CodeRabbit.MyProject
+{
+    public class MathUtilities
+    {
+        public double SquareRoot(double value)
+        {
+            if (value < 0)
+                throw new ArgumentOutOfRangeException(nameof(value), "Value must be non-negative.");
+
+            return System.Math.Sqrt(value);
+        }
+
+        public bool TrySquareRoot(double value, out double result)
+        {
+            if (value < 0 || double.IsNaN(value) || double.IsInfinity(value))
+            {
+                result = double.NaN;
+                return false;
+            }
+
+            result = System.Math.Sqrt(value);
+            return true;
+        }
+    }
+}
